cc0a4c6fa554b49b26a2c2b12fe194d77305438e
UrosTatomir <tatomir.uros@gmail.com>
2019-03-13 21:38:29 +0100

    Update commit bootstrap css js

--- view/index.php
+++ view/index.php
@@ -1,12 +1,11 @@
 <?php
-include '../includes/header.php'; ?>
-
-<?php include '../includes/nav.php';
+include '../includes/header.php'; 
+include '../includes/nav.php';
 ?>
 
 <!-- <div class="card bg-dark text-white"> -->
 <div class="row justify-content-md">
-    <img src="../images/erosion1.jpg" class="card-img" width="1280" alt="card image" />
+    <img src="../images/erosion1.jpg" class="card-img" width="1330" alt="card image" />
 
     <div class="card-img-overlay">
         <br><br>
